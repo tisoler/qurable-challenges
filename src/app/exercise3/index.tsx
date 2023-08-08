abb47fb55f7de32059add4ecc42973dff9d383dd
Tisoler <diegompaz@gmail.com>
2023-08-08 04:18:11 -0300

    fix: exercise 3 - end time greater than start time

--- src/app/exercise3/index.tsx
+++ src/app/exercise3/index.tsx
@@ -10,9 +10,10 @@ type Tuple = [number, number]
 type MeetingArray = Tuple[]
 
 const ExerciseThree = ({ onGoBack }: ExerciseTwoProps) => {
-  const [meetingArray, setMeetingArray] = useState<MeetingArray>([])
+  const [meetingArray, setMeetingArray] = useState<MeetingArray>([[8,9], [9,10], [9,11], [8.5, 9], [7.5,8.5], [11,12]])
   const [bestSchedule, setBestSchedule] = useState<MeetingArray>()
   const [meeting, setMeeting] = useState<{ start?: number, end?: number }>()
+  const [warning, setWarning] = useState<string>()
 
   const handleChangeStart = (e: ChangeEvent<HTMLInputElement>) => {
     const value = e?.target?.value
@@ -29,7 +30,13 @@ const ExerciseThree = ({ onGoBack }: ExerciseTwoProps) => {
   }
 
   const handleAddTuple = () => {
+    setWarning('')
     if (!meeting?.start || !meeting.end) return
+
+    if (meeting.start >= meeting.end) {
+      setWarning('End time must be greater than start time.')
+      return
+    }
 
     setMeetingArray([
       ...meetingArray,
@@ -82,36 +89,39 @@ const ExerciseThree = ({ onGoBack }: ExerciseTwoProps) => {
       <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
         Enter a list of meetings with start and end time (e.g. (9,10) - (11,12.5))
       </label>
-      <div className="w-full sm:w-11/12 md:w-8/12 flex items-center justify-center">
-        {'('}
-        <input
-          type="number"
-          className="shadow appearance-none border rounded w-2/12 sm:w-1/12 py-2 px-3 m-1 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          onChange={handleChangeStart}
-          value={meeting?.start || ''}
-        />
-        {','}
-        <input
-          type="number"
-          className="shadow appearance-none border rounded w-2/12 sm:w-1/12 py-2 px-3 m-1 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          onChange={handleChangeEnd}
-          value={meeting?.end || ''}
-        />
-        {')'}
-        <button
-          className={`bg-${!meeting?.start || !meeting.end ? 'gray' : 'green'}-500 ${meeting?.start && meeting.end && 'hover:bg-green-700'} text-white font-bold ml-4 py-2 px-4 rounded w-3/12 sm:w-2/12 h-20 m-2`}
-          disabled={!meeting?.start || !meeting.end}
-          onClick={handleAddTuple}
-        >
-          Add tuple
-        </button>
-        <button
-          className={`bg-${!meetingArray.length ? 'gray' : 'green'}-500 ${meetingArray?.length && 'hover:bg-green-700'} text-white font-bold ml-4 py-2 px-4 rounded w-3/12 sm:w-2/12 h-20 m-2`}
-          disabled={!meetingArray?.length}
-          onClick={() => setMeetingArray([])}
-        >
-          Clean
-        </button>
+      <div className="w-full sm:w-11/12 md:w-8/12 flex flex-col">
+        <div className="w-full flex items-center justify-center">
+          {'('}
+          <input
+            type="number"
+            className="shadow appearance-none border rounded w-2/12 sm:w-1/12 py-2 px-3 m-1 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            onChange={handleChangeStart}
+            value={meeting?.start || ''}
+          />
+          {','}
+          <input
+            type="number"
+            className="shadow appearance-none border rounded w-2/12 sm:w-1/12 py-2 px-3 m-1 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            onChange={handleChangeEnd}
+            value={meeting?.end || ''}
+          />
+          {')'}
+          <button
+            className={`bg-${!meeting?.start || !meeting.end ? 'gray' : 'green'}-500 ${meeting?.start && meeting.end && 'hover:bg-green-700'} text-white font-bold ml-4 py-2 px-4 rounded w-3/12 sm:w-2/12 h-20 m-2`}
+            disabled={!meeting?.start || !meeting.end}
+            onClick={handleAddTuple}
+          >
+            Add tuple
+          </button>
+          <button
+            className={`bg-${!meetingArray.length ? 'gray' : 'green'}-500 ${meetingArray?.length && 'hover:bg-green-700'} text-white font-bold ml-4 py-2 px-4 rounded w-3/12 sm:w-2/12 h-20 m-2`}
+            disabled={!meetingArray?.length}
+            onClick={() => setMeetingArray([])}
+          >
+            Clean
+          </button>
+        </div>
+        {warning && <label className="block w-full text-center text-red-500 text-xs font-bold">{warning}</label>}
       </div>
       <div className="w-full sm:w-11/12 md:w-8/12 flex items-center justify-center">
         <label className="block uppercase w-2/12 tracking-wide text-gray-700 text-xs font-bold mr-2">Meetings:</label>
